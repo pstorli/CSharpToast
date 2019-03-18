d5656c3f489d68c54e63b316e570f3b61f6fb20c
pstorli <pstorli@blm.gov>
2019-03-18 15:30:56 -0700

    Initial Version

---
+++ CSharpToast/MainWindow.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace CSharpToast
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click (object sender, EventArgs e)
+        {
+            Toast.show ("Toast is done!");
+        }
+    }
+}
---
+++ CSharpToast/Toast.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Timers;
+using System.Windows.Forms;
+namespace CSharpToast
+{    
+    public partial class Toast : Form {
+
+        public static int DEFAULT_MS_DELAY = 2500;
+        private delegate void SafeOnTimedEvent(Object source, ElapsedEventArgs e);
+
+        public Toast(String message)
+        {
+            InitializeComponent();
+
+            Message.Text = message;
+        }
+
+        public static Toast show (String message)
+        {
+            return show(message, DEFAULT_MS_DELAY);
+        }
+
+        public static Toast show (String message, int ms)
+        {
+            Toast toast = new Toast(message);
+            System.Timers.Timer aTimer = new System.Timers.Timer(ms);
+            aTimer.Elapsed += toast.OnTimedEvent;
+            aTimer.AutoReset = false;
+            aTimer.Enabled = true;
+            toast.ShowDialog();
+
+            return toast;
+        }
+
+        private void OnTimedEvent (Object source, ElapsedEventArgs e)
+        {
+            if (this.InvokeRequired)
+            {
+                var d = new SafeOnTimedEvent(OnTimedEvent);
+                Invoke(d, new object[] { source, e });
+            }
+            else
+            {
+                Close();
+            }
+        }        
+    }
+}
